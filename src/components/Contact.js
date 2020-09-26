dfeb43d7147935730b583f362b826d0ab7b2a7c1
Anne Quinkenstein <a.quinkenstein@gmail.com>
2020-09-26 11:52:54 +0200

    basic form

---
+++ src/components/Contact.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+import { TextField, Typography, Button, Grid, Box } from "@material-ui/core";
+import SendIcon from "@material-ui/icons/Send";
+import Navbar from "./Navbar";
+
+const InputField = withStyles({
+  root: {
+    "& label.Mui-focused": {
+      color: "tomato",
+    },
+    "& label": {
+      color: "tan",
+    },
+    "&.MuiOutlinedInput-root": {
+        "& fieldset" : {
+            borderColor: "tan",
+        },
+        '&:hover fieldset': {
+            borderColor: 'tan', 
+        },
+        '&.Mui-focused fieldset': {
+            borderColor: 'tan',
+        },
+    }
+  },
+})(TextField);
+
+const Contact = () => {
+  return (
+    <>
+      <Navbar />
+      <Box component="div">
+        <Grid container justify="center">
+          <Box component="form">
+            <Typography variant="h5">hire or contact me ...</Typography>
+            <InputField fullWidth={true} label="Name" variant="outlined" />
+            <InputField fullWidth={true} label="Email" variant="outlined" />
+            <InputField fullWidth={true} label="Company name" variant="outlined" />
+          </Box>
+        </Grid>
+      </Box>
+    </>
+  );
+};
+
+export default Contact;
