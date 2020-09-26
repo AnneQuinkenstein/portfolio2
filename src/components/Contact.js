fa933b971b2f8e07cd0f043ad923f4cca1846685
Anne Quinkenstein <a.quinkenstein@gmail.com>
2020-09-26 17:37:41 +0200

    updated ReadMe

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,19 +4,19 @@ import { TextField, Typography, Button, Grid, Box } from "@material-ui/core";
 import SendIcon from "@material-ui/icons/Send";
 import Navbar from "./Navbar";
 
-const useStyles=makeStyles(theme=> ({
-form: {
-    top: '50%', 
-    left: '50%', 
-    position: 'absolute', 
-    transform: 'translate(-50%, -50%)',
-},
-button: {
-    marginTop: '1rem', 
-    color: 'tomato', 
-    borderColor: 'tomato', 
-}
-}))
+const useStyles = makeStyles((theme) => ({
+  form: {
+    top: "50%",
+    left: "50%",
+    position: "absolute",
+    transform: "translate(-50%, -50%)",
+  },
+  button: {
+    marginTop: "1rem",
+    color: "tomato",
+    borderColor: "tomato",
+  },
+}));
 
 const InputField = withStyles({
   root: {
@@ -27,43 +27,73 @@ const InputField = withStyles({
       color: "#3080d6",
     },
     "& .MuiOutlinedInput-root": {
-        "& fieldset" : {
-            borderColor: "#3080d6",
-        },
-        '&:hover fieldset': {
-            borderColor: '#3080d6', 
-        },
-        '&.Mui-focused fieldset': {
-            borderColor: '#3080d6',
-        },
-    }
+      "& fieldset": {
+        borderColor: "#3080d6",
+      },
+      "&:hover fieldset": {
+        borderColor: "#3080d6",
+      },
+      "&.Mui-focused fieldset": {
+        borderColor: "#3080d6",
+      },
+    },
   },
 })(TextField);
 
 const Contact = () => {
-
-const classes = useStyles(); 
+  const classes = useStyles();
 
   return (
     <>
-      <Navbar />
-      <Box component="div" style={{background: "#222833d4", height: '100vh'}}>
+      <Box component="div" style={{ background: "#222833d4", height: "100vh" }}>
+        <Navbar />
         <Grid container justify="center">
           <Box component="form" className={classes.form}>
-            <Typography variant="h5" style={{color: 'tomato', textAlign: 'center', textTransform: "uppercase"}}>hire or contact me ...</Typography>
-            <InputField fullWidth={true} label="Name" variant="outlined" inputProps={{style: { color: "white"}}} margin="dense" size="medium" />
-              <br/>
-            <InputField fullWidth={true} label="Email" variant="outlined" 
-            inputProps={{style: { color: "white"}}}
-            margin="dense" size="medium" />
-            <br/>
-            <InputField fullWidth={true} label="Company name" variant="outlined"
-            inputProps={{style: { color: "white"}}}
-            margin="dense" size="medium"  />
-          <br/>
-          <Button className={classes.button}variant="outlined" fullWidth={true} endIcon={<SendIcon/>}>
+            <Typography
+              variant="h5"
+              style={{
+                color: "tomato",
+                textAlign: "center",
+                textTransform: "uppercase",
+              }}
+            >
+              hire or contact me ...
+            </Typography>
+            <InputField
+              fullWidth={true}
+              label="Name"
+              variant="outlined"
+              inputProps={{ style: { color: "white" } }}
+              margin="dense"
+              size="medium"
+            />
+            <br />
+            <InputField
+              fullWidth={true}
+              label="Email"
+              variant="outlined"
+              inputProps={{ style: { color: "white" } }}
+              margin="dense"
+              size="medium"
+            />
+            <br />
+            <InputField
+              fullWidth={true}
+              label="Company name"
+              variant="outlined"
+              inputProps={{ style: { color: "white" } }}
+              margin="dense"
+              size="medium"
+            />
+            <br />
+            <Button
+              className={classes.button}
+              variant="outlined"
+              fullWidth={true}
+              endIcon={<SendIcon />}
+            >
               contact me
-          </Button>
+            </Button>
           </Box>
         </Grid>
       </Box>
